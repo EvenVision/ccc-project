4ad914f9adc412d05c8e558e6fecfdbf7747e4f3
Sergei Porfenovich <sergei.porfenovich@gmail.com>
2022-12-16 18:41:42 +0300

    Changes: - Add custom theme from bootstrap5 theme

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$xmlStr = file_get_contents('response.xml');
-$xml = new SimpleXMLElement($xmlStr);
-$res = $xml->xpath('/VehicleDamageEstimateAddRq/DocumentInfo/DocumentID');
-print_r($res);
